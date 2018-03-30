2ac376c6428c1a42fc3ffad49de266cf4f3c3893
Sahana Balram <sahana2008balram@gmail.com>
2018-03-30 14:33:45 -0700

    Create a check to the time

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,7 +1,20 @@
 module.exports = function (app) {
     // GET call to return JSON that formats natural and unix date
     app.get("/date/:value", function (request, response, next) { 
+        // Gets the request for data for date
         let value = request.params.value;
-        response.json({unix: value});
+        // Formating date in natural date view
+        let naturalDateFormat = {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        };
+// if its not a number then it is a string value and need to create a natural date
+        if(isNaN(value)){
+            let naturalDate = new Date(value);
+            naturalDate = naturalDate.toLocaleDateString("en-us",naturalDateFormat);
+            let unixDate = new Date(value).getTime()/ 1000;
+        }
+        response.json({unix: unixDate, natural: naturalDate});
     });
 }
