08d04907937a0a004583410183ca4db06be29e0d
Sahana Balram <sahana2008balram@gmail.com>
2018-03-30 14:04:20 -0700

    Require the dependencies

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
