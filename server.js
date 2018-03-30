49be48f0054037b82a170adba95c1d56ef7b18e7
Sahana Balram <sahana2008balram@gmail.com>
2018-03-30 14:09:06 -0700

    Add port details

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
-
+const app = module.exports = express();
+var PORT = process.env.PORT || 3009;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(cors());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
