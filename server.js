af40d571c4a45e3bb1978a1f6170a298164a2543
Sahana Balram <sahana2008balram@gmail.com>
2018-03-30 14:27:09 -0700

    Server code

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(cors());
-
+require("./app/routes/apiRoutes.js")(app);
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
