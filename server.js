768d4c433e890aba827e0ab3f61d7144b979a0a4
Sahana Balram <sahana2008balram@gmail.com>
2018-03-30 15:05:46 -0700

    Add html routing

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = module.exports = express();
 var PORT = process.env.PORT || 3009;
+
+app.use(express.static('app/public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(cors());
+
 require("./app/routes/apiRoutes.js")(app);
+require("./app/routes/htmlRoutes.js")(app);
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
